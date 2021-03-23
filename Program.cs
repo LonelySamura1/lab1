243dd5b8402cf256ea1ac6b9c4d667ce4ebe8bc3
Бурень Павел Юрьевич <318226@college.local>
2021-03-23 08:43:47 +0300

    code

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Gun> GunList = new List<Gun>();
+            while (true)
+            {
+                Console.WriteLine("1. добавить новый");
+                Console.WriteLine("2. Вывести всё");
+                ConsoleKeyInfo PressedKey = Console.ReadKey();
+                if (PressedKey.KeyChar == '1')
+                {
+                    Console.WriteLine("Введите название: ");
+                    string Name = Console.ReadLine();
+                    Console.WriteLine("Введите калибр: ");
+                    float Caliber = Convert.ToSingle(Console.ReadLine());
+                    Console.WriteLine("Введите дальность: ");
+                    double Range = Convert.ToDouble(Console.ReadLine());
+                    GunList.Add(new Gun(Name, Caliber, Range));
+                    Console.Clear();
+                }
+                if (PressedKey.KeyChar == '2')
+                {
+                    for (int i = 0; i < GunList.Count; i++)
+                    {
+                        Console.WriteLine("Название: " + GunList[i].Name + " калибр: " + GunList[i].Caliber + " дальность: " + GunList[i].Range);
+                    }
+                }
+            }
+        }
+    }
+}
